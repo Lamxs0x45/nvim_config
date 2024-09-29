6901ad120a7ade02c7b48d196a744f11800531d5
Lamxs0x45 <72706049+Lamxs0x45@users.noreply.github.com>
2024-09-29 22:04:19 +0200

    some more changes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,9 +1,10 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+vim.cmd("set list")
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -1,47 +1,49 @@
 require("mason").setup()
 
-local servers = { "clangd", "lua_ls", "pyright", "cssls", "tsserver", "gopls", "rust_analyzer", "hls"}
+local servers = { "lua_ls", "pyright", "cssls", "tsserver", "gopls", "rust_analyzer" }
 
 require("mason-lspconfig").setup {
-  ensure_installed = servers,
+    ensure_installed = servers,
 }
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local on_attach = function(_, _)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-  vim.keymap.set('n', '<space>f', function()
-    vim.lsp.buf.format { async = true }
-  end, {})
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+    vim.keymap.set('n', '<space>f', function()
+        vim.lsp.buf.format { async = true }
+    end, {})
 end
 local lspconfig = require "lspconfig"
 
 local clangd_cmd = {
-  "clangd",
-  "--background-index",
-  "--clang-tidy",
-  "--header-insertion=iwyu",
-  "--fallback-style=llvm",
-  "--header-insertion=never",
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--fallback-style=llvm",
+    "--function-arg-placeholders=false",
+    "--header-insertion=never",
 }
 
+lspconfig["hls"].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
+lspconfig["clangd"].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = clangd_cmd,
+}
 for _, lsp in ipairs(servers) do
-  if lsp == "clangd" then
     lspconfig[lsp].setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-      cmd = clangd_cmd,
+        on_attach = on_attach,
+        capabilities = capabilities,
     }
-  else
-    lspconfig[lsp].setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-  end
 end
